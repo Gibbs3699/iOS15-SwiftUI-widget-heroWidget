6181384e1596f0ca0659d98b9a525f171815beab
KAMONPAN KAEWHEANG <kamonpan@dit.daikin.co.jp>
2022-04-06 17:02:25 +0700

    Initial Commit

---
+++ HeroWidget/HeroWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeroWidgetApp.swift
+//  HeroWidget
+//
+//  Created by backup on 6/4/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct HeroWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
