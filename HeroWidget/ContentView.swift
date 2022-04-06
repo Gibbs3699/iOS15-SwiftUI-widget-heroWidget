054e98cc2a9801b89dfca7b07fb2e587f4f1f176
Gibbs3699 <gift_kamonpan@hotmail.com>
2022-04-06 23:20:50 +0700

    Saving data to app storage

--- HeroWidget/ContentView.swift
+++ HeroWidget/ContentView.swift
@@ -10,11 +10,28 @@ import SwiftUI
 let superHeroArray = [spiderman, captain, groot]
 
 struct ContentView: View {
+    
+    @AppStorage("hero", store: UserDefaults(suiteName: "group.Kamonpan.mac.HeroWidget"))
+    
+    var heroData : Data = Data()
+    
     var body: some View {
         VStack {
             ForEach(superHeroArray) { hero in
                 HeroView(hero: hero)
+                    .onTapGesture {
+                     saveToDefaults(hero: hero)
+                }
             }
+        }
+    }
+    
+    func saveToDefaults(hero: SuperHero) {
+//        print(hero.name)
+        
+        if let heroData = try? JSONEncoder().encode(hero) {
+            self.heroData = heroData
+            print(hero.name)
         }
     }
 }
